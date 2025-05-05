ec3e98adea83a7a7d69e3fa9b527d470466a1744
RoryCameron <rorycameron725@gmail.com>
2025-05-05 18:50:19 +0100

    - Added API call to check blacklisted IPS - Added saving checked IPs to file malicious or not

--- packetSniffer.py
+++ packetSniffer.py
@@ -1,29 +1,82 @@
 
 from scapy.all import *
 import time
+import os
+from dotenv import load_dotenv
+import requests
+import pickle
 
-blacklist = ["192.168.178.24"]
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+WIFI_INTERFACE = os.getenv("WIFI_INTERFACE")
+URL = os.getenv("URL")
 
 
-def show_packet(packet):
+def saveCheckedIps():
+    with open("checkIPs.pkl", "wb") as f:
+        pickle.dump(checked_ips, f)
 
-    global sniffer
+def loadCheckedIps():
+    
+    global checked_ips
+
+    try:
+        with open("checkIPs.pkl", "rb") as f:
+            checked_ips = pickle.load(f)
+    except FileNotFoundError:
+        checked_ips = {}
+
+loadCheckedIps()
+
+def checkIP(ip):
+    
+    if ip in checked_ips:
+        return checked_ips[ip]
+
+    headers = {
+        "Key": API_KEY,
+        "Accept": "application/json"
+    }
+
+    params = {
+        "ipAddress": ip,
+        "maxAgeInDays": 90
+    }
+
+    try:
+        response = requests.get(URL, headers=headers, params=params)
+    except requests.exceptions.RequestException as e:
+        print(f"Error checking IP {ip}: {e}")
+
+
+    if response.status_code == 200:
+
+        data = response.json()
+        is_malicious = data['data']['abuseConfidenceScore'] > 50 # Confidence threshhold
+        checked_ips[ip] = is_malicious
+        saveCheckedIps()
+        return is_malicious # Suspicious IP
+            
+    return False
+
+
+
+def showPacket(packet):
 
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
     if packet.haslayer(IP):
+
         source_ip = packet[IP].src
         dest_ip = packet[IP].dst
 
+        if checkIP(source_ip):
+            print("\n====== WARNING: IP: {} Reported as Suspicious ======".format(source_ip))
 
-        if source_ip in blacklist or dest_ip in blacklist:
-            print("\n======\n WARNING: SUS PING FROM IP:{}\n======".format(source_ip))
+        if checkIP(dest_ip):
+            print("\n====== WARNING: IP: {} Reported as Suspicious ======".format(dest_ip))
                 
-
-
     print("{} {}".format(timestamp, packet.summary())) # Summary of packet
 
 
-iface = "\\Device\\NPF_{6CF310CB-2858-4450-9F0F-1C447815573E}" # WIFI Interface
-
-sniff(iface=iface, prn=show_packet, store=False, promisc=True)
+sniff(iface=WIFI_INTERFACE, prn=showPacket, store=False, promisc=True)
